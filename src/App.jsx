e8c8f1fec731b663e6fcbd334c8d17f378eb805a
Ivan <135625657+IvanSantos1989@users.noreply.github.com>
2024-08-20 14:24:32 +0100

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
+export default App
+
+
