55e4c95cd99a8342044ad6590926ced985174cd4
Ivan <135625657+IvanSantos1989@users.noreply.github.com>
2024-09-13 04:43:44 +0100

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LRProperties_frontEnd/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
