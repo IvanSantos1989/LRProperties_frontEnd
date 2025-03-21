59a098cc7d9d996b33dab3dd752976d030018cd7
Guilherme Santos <guilhermefsantosn@gmail.com>
2025-03-21 00:53:56 +0000

    landing page visual fixes, added error modal

--- src/Components/Home/NewestDeals.jsx
+++ src/Components/Home/NewestDeals.jsx
@@ -6,9 +6,8 @@ const NewestDeals = () => {
 
     return (
         <div className='p-8 mx-auto  flex flex-col  items-center'>
-            <h1 className='text-6x1 font-bold flex justify-center mb-8'>Available Accommodation List</h1>        
+            <h1 className='text-2xl font-bold flex justify-center mb-8 mt-10'>Available Accommodation List</h1>        
             <HostelList />
-
             <AboutUs />
         </div>
 
--- src/Components/micro/SearchBar.jsx
+++ src/Components/micro/SearchBar.jsx
@@ -3,6 +3,7 @@ import { FiSearch } from "react-icons/fi";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import GuestSelector from "./GuestSelector";
+import ErrorModal from "./Modals/ErrorModal";
 
 const SearchBar = () => {
   const [checkInDate, setCheckInDate] = useState(null);
@@ -10,7 +11,10 @@ const SearchBar = () => {
   const [showGuestSelector, setShowGuestSelector] = useState(false);
   const [adults, setAdults] = useState(1);
   const [pets, setPets] = useState(0);
-  const [errorMessage, setErrorMessage] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("The minimum stay is 2 nights. Please check your fields and try again!");
+  const [erroModalVisible, setErrorModalVisible] = useState(false);
+
   const guestSelectorRef = useRef(null);
 
   const handleClickOutside = (event) => {
@@ -30,82 +34,90 @@ const SearchBar = () => {
   }, []);
 
   const handleSearch = () => {
-    const nights = checkInDate && checkOutDate ? (checkOutDate - checkInDate) / (1000 * 60 * 60 * 24) : 0;
+    const nights =
+      checkInDate && checkOutDate
+        ? (checkOutDate - checkInDate) / (1000 * 60 * 60 * 24)
+        : 0;
     if (nights < 2) {
-      setErrorMessage("Minimum stay is 2 nights");
+      setErrorMessage("The minimum stay is 2 nights. Please check your fields and try again!");
     } else if (pets > 2) {
-      setErrorMessage("2 pets allowed only");
+      setErrorMessage("2 pets allowed only.");
     } else {
       setErrorMessage("");
       // Execute search logic here
     }
+
+    setErrorModalVisible(true)
+
   };
 
   return (
-    <div className="flex items-center justify-between bg-white p-4 rounded-full shadow-lg">
-      <div className="flex items-center gap-3 border-r pr-3">
-        <div className="flex flex-col">
-          <label className="text-xs outline-none">PORTO</label>
-          {/* <input
-                        type="text"
-                        placeholder="Adicionar localização"
-                        className="outline-none"
-                    /> */}
+    <div>
+      <ErrorModal errorMessage={errorMessage} visible={erroModalVisible} setVisible={setErrorModalVisible}/>
+      <div className="flex items-center justify-between bg-white p-4 rounded-full shadow-lg">
+        <div className="flex items-center gap-3 border-r pr-3">
+          <div className="flex flex-col">
+            <label className="text-xs outline-none">PORTO</label>
+            {/* <input
+                  type="text"
+                  placeholder="Adicionar localização"
+                  className="outline-none"
+              /> */}
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-3 border-r pr-3">
-        <div className="flex flex-col ml-4">
-          <label className="text-xs text-gray-500">Check-in</label>
-          <DatePicker
-            selected={checkInDate}
-            onChange={(date) => setCheckInDate(date)}
-            placeholderText="Add date"
-            className="outline-none"
-            dateFormat={"dd/MM/yyyy"}
-          />
+        <div className="flex items-center gap-3 border-r pr-3">
+          <div className="flex flex-col ml-4">
+            <label className="text-xs text-gray-500">Check-in</label>
+            <DatePicker
+              selected={checkInDate}
+              onChange={(date) => setCheckInDate(date)}
+              placeholderText="Add date"
+              className="outline-none"
+              dateFormat={"dd/MM/yyyy"}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-3 border-r pr-3">
-        <div className="flex flex-col ml-4">
-          <label className="text-xs text-gray-500">Check-out</label>
-          <DatePicker
-            selected={checkOutDate}
-            onChange={(date) => setCheckOutDate(date)}
-            placeholderText="Add date"
-            className="outline-none"
-            dateFormat={"dd/MM/yyyy"}
-          />
+        <div className="flex items-center gap-3 border-r pr-3">
+          <div className="flex flex-col ml-4">
+            <label className="text-xs text-gray-500">Check-out</label>
+            <DatePicker
+              selected={checkOutDate}
+              onChange={(date) => setCheckOutDate(date)}
+              placeholderText="Add date"
+              className="outline-none"
+              dateFormat={"dd/MM/yyyy"}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-3">
-        <div className="flex flex-col relative ml-4">
-          <label className="text-xs text-gray-500">Who</label>
-          <input
-            type="text"
-            value={`${adults} Guest, ${pets} Animal`}
-            onFocus={() => setShowGuestSelector(true)}
-            onChange={(e) => {}}
-            className="outline-none"
-          />
-          {showGuestSelector && (
-            <div ref={guestSelectorRef}>
-              <GuestSelector
-                adults={adults}
-                pets={pets}
-                setAdults={setAdults}
-                setPets={setPets}
-              />
-            </div>
-          )}
+        <div className="flex items-center gap-3">
+          <div className="flex flex-col relative ml-4">
+            <label className="text-xs text-gray-500">Who</label>
+            <input
+              type="text"
+              value={`${adults} Guest, ${pets} Animal`}
+              onFocus={() => setShowGuestSelector(true)}
+              onChange={(e) => {}}
+              className="outline-none"
+            />
+            {showGuestSelector && (
+              <div ref={guestSelectorRef}>
+                <GuestSelector
+                  adults={adults}
+                  pets={pets}
+                  setAdults={setAdults}
+                  setPets={setPets}
+                />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <button
-        className="bg-[#FF9874] p-3 rounded-full text-white hover:bg-[#E57A5a]"
-        onClick={handleSearch}
-      >
-        <FiSearch />
-      </button>
-      {errorMessage && <p className="text-red-500 text-sm mt-2">{errorMessage}</p>}
+        <button
+          className="bg-[#FF9874] p-3 rounded-full text-white hover:bg-[#E57A5a]"
+          onClick={handleSearch}
+        >
+          <FiSearch />
+        </button>
+      </div>      
     </div>
   );
 };
