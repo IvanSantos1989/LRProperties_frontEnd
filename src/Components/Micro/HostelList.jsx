c486760fe26cebe771377c619b1198374a247b5b
Guilherme Santos <guilhermefsantosn@gmail.com>
2025-04-01 20:17:50 +0100

    small fixes to the project

--- src/Components/Micro/HostelList.jsx
+++ src/Components/Micro/HostelList.jsx
@@ -1,7 +1,7 @@
 import { fetchHostels } from '@/api/house';
 import React, { useEffect, useState } from 'react';
-import LoadingSpinner from './LoadingSpinner';
 import HostelCard from '../Home/Card';
+import LoadingSpinner from './Spinners/LoadingSpinner';
 
 function HostelList() {
     const [hostels, setHostels] = useState([]);
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NewestDeals from '@/Components/Home/NewestDeals';
 import Search from '@/Components/Home/Search';
-import { Button } from '@/Components/micro/Buttons/Button'; // Ajustado para o caminho correto
 
 const Home = () => {
     return (
